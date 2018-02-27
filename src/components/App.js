dda503344e89f898534c67255bd51c4a7a153411
Hari Akunuri <harakunu@wkmus5887804.global.publicisgroupe.net>
2018-02-27 16:38:44 -0500

    added header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './header/Header';
 
 class App extends React.Component{
     render(){
         return(
             <div>
-            <h2> The Childrens Place </h2>
+            <Header/>
             </div>
         );
     }
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div>
+            <p> The Childrens Place </p>
+            
+            </div>
+        );
+    }
+}
+export default Header;
